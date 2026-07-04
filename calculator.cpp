1c5ee35f95f33b1fbfcf03e572aa9f10c1be1af9
priyanshssm96933-pixel <priyanshssm96933@gmail.com>
2026-07-05 02:23:39 +0530

    Implement basic calculator functionality
    Fix input prompt typo and improve output formatting.

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a , b;
+    cout <<"enter your first number: ";
+    cin >> a;
+    cout <<"ener your second number: ";
+    cin >> b;
+    cout <<" addition = " << a+b << endl;
+    cout <<" subtraction = " << a-b << endl;
+    cout <<" multiplication = " << a*b << endl;
+     cout <<" division = " << a/b << endl;
+     cout <<" thank you priyansh!"<< endl;
+     return 0;
+}
+   
+
